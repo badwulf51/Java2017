ed203b4a6ddf3b650ed1ab7ace04f10d6da40e07
badwulf51 <aronomalley@yahoo.com>
2018-01-11 19:26:00 +0000

    Added comments and fixes

--- test/src/ie/gmit/sw/main/Runner.java
+++ test/src/ie/gmit/sw/main/Runner.java
@@ -1,10 +1,14 @@
+// Aron O' Malley 
+// G00327019 
+// Java Project 2017 
+
 package ie.gmit.sw.main;
-
+// Runner class, runs the main menu that is displayed in console 
 public class Runner {
-
+	// Displays menu 
 	public static void main(String[] args) throws Exception {
 		Menu.main();
-
+		
 	}
 
 }
