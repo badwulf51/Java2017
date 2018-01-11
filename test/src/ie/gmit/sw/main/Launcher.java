395acd4e0890618b803e86e530b04c771612f7c2
badwulf51 <aronomalley@yahoo.com>
2018-01-11 19:28:17 +0000

    Added comments and fixes

--- test/src/ie/gmit/sw/main/Launcher.java
+++ test/src/ie/gmit/sw/main/Launcher.java
@@ -1,12 +1,16 @@
+// Aron O' Malley 
+// G00327019 
+// Java Project 2017 
+
 package ie.gmit.sw.main;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-
+// class for launcher which assists with shingles 
 public class Launcher {
-	
+	// file 1 and 2 
 	public static void Launch(String f1, String f2) {
-		
+		// attempt at blocking queue for shingls and hashes
 		BlockingQueue<Shingle> q = new LinkedBlockingQueue<>(100);
 		// threadPoolSize	
 		
@@ -17,20 +21,20 @@ public class Launcher {
 		t1.start();
 		t2.start();
 		t3.start();
-		
+		// message for when thread 1 is exited
 		try {
 			t1.join();
 			System.out.println("Launcher: Thread 1 exited...");
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+		
 			e.printStackTrace();
 		}
-		
+		// message for when thread 2 is exited
 		try {
 			t2.join();
 			System.out.println("Launcher: Thread 2 exited...");
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 		
