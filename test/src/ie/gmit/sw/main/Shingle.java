77ae05a476c5065d21c893f9f4a586a8e492a854
badwulf51 <aronomalley@yahoo.com>
2018-01-11 19:25:28 +0000

    Added comments and some fixes

--- test/src/ie/gmit/sw/main/Shingle.java
+++ test/src/ie/gmit/sw/main/Shingle.java
@@ -1,26 +1,36 @@
-package ie.gmit.sw.main;
+// Aron O' Malley 
+// G00327019 
+// Java Project 2017 
 
+// Pakcages and imports
+package ie.gmit.sw.main;
+// basic getters and setters class 
 public class Shingle {
 	private int docId;
 	private int hashcode;
-	
+	// super class 
 	public Shingle(int docId, int hashcode) {
 		super();
 		this.docId = docId;
 		this.hashcode = hashcode;
 	}
-	
+	// gets docID
 	public int getDocId() {
 		return docId;
 	}
+	// sets docID
 	public void setDocId(int docId) {
 		this.docId = docId;
 	}
+	// gets hashcode
 	public int getHashcode() {
 		return hashcode;
 	}
+	
+	// Sets hashcode 
 	public void setHashcode(int hashcode) {
 		this.hashcode = hashcode;
 	}
 
+	
 }
