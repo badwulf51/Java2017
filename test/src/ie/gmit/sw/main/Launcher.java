b7e75c27a1641dfbaf957047c4ead686ccb44472
badwulf51 <aronomalley@yahoo.com>
2018-01-11 16:30:48 +0000

    Launcher semi working

--- test/src/ie/gmit/sw/main/Launcher.java
+++ test/src/ie/gmit/sw/main/Launcher.java
@@ -6,11 +6,12 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class Launcher {
 	
 	public static void Launch(String f1, String f2) {
+		
 		BlockingQueue<Shingle> q = new LinkedBlockingQueue<>(100);
 		// threadPoolSize	
-
-		Thread t1 = new Thread(new DocumentParser(f1, q, 5, 5), "T1");
-		Thread t2 = new Thread(new DocumentParser(f2, q, 5, 5), "T2");
+		
+		Thread t1 = new Thread(new DocumentParser(f1, q, 5, 5, 0), "T1");
+		Thread t2 = new Thread(new DocumentParser(f2, q, 5, 5, 1), "T2");
 		Thread t3 = new Thread(new MainProcess(q, 5, 5), "T3");
 		
 		t1.start();
@@ -19,6 +20,7 @@ public class Launcher {
 		
 		try {
 			t1.join();
+			System.out.println("Launcher: Thread 1 exited...");
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -26,9 +28,18 @@ public class Launcher {
 		
 		try {
 			t2.join();
+			System.out.println("Launcher: Thread 2 exited...");
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		try {
+			t3.join();
+			System.out.println("Launcher: MainProcess exited...");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
 }
